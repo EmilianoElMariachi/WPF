e21a21e068ae4850910081bab1ab2b7c7f13387f
EmilianoElMariachi <milhouzelatarlouze@msn.com>
2015-02-16 14:55:38 +0100

    ELM : wip ; IRecorder

---
+++ ElMariachi.WPF.Tools/Modelling/ModelRecording/PrivateClasses/IRecorder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ElMariachi.WPF.Tools.UndoRedo.RevertibleCommands;
+
+namespace ElMariachi.WPF.Tools.Modelling.ModelRecording.PrivateClasses
+{
+    interface IRecorder
+    {
+        bool CanRecordPropertyChange { get; }
+
+        void RecordPropertyChange(Property property, IRevertibleCommand revertibleCommand);
+
+
+    }
+
+    internal class Property
+    {
+
+        public int Delay { get; set; }
+
+        public string RecordGroupName { get; set; }
+
+    }
+}
